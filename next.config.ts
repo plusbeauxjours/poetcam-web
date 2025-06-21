7355711ed907a7fb67401a4e382aa4c53e0a90cf
plusbeauxjours <plusbeauxjours@gmail.com>
2025-06-21 20:14:05 +0900

    fix: allow camera usage

--- next.config.ts
+++ next.config.ts
@@ -50,7 +50,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=()",
+            value: "camera=(self), microphone=(), geolocation=()",
           },
         ],
       },
